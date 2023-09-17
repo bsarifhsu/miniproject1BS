0855c6aaf8bf339002e025855a747a60d38d34eb
bsarifhsu <b_sari@mail.fhsu.edu>
2023-09-17 14:50:43 -0500

    The plots generated for 5 stocks.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
-import pprint
 
 # Ticker for Apple is APPL
 # Ticker for Microsoft is MSFT
@@ -27,27 +26,30 @@ def getClosing(ticker):
         closing_list.append(round(price,2))
     return closing_list
 
-msftClosing = np.array(getClosing("MSFT"))
-days = list(range(1, len(msftClosing)+1))
 
-#It plots the graph
-plt.plot(msftClosing)
+stocks = ["MSFT","SONY","ADBE","GOOG","NVDA"]
+for stock in stocks:
+    stockClosing = np.array(getClosing(stock))
+    days = list(range(1, len(stockClosing)+1))
 
-#Get the min and max for Y axis
-prices = getClosing("MSFT")
-prices.sort()
-low_price = prices[0]
-high_price = prices[-1]
+    # It plots the graph
+    plt.plot(days, stockClosing)
 
-#Set the X axis min and max
-#Form [xmin,xmax, ymin, ymax]
-plt.axis([10,1,low_price-2,high_price+2])
+    # Get the min and max for Y axis
+    prices = getClosing(stock)
+    prices.sort()
+    low_price = prices[0]
+    high_price = prices[-1]
 
-#Set the labels for the graph
-plt.xlabel("Days Ago")
-plt.ylabel("Closing Price")
-plt.title("Closing Price for MSFT")
+    # Set the X axis min and max
+    # Form [xmin,xmax, ymin, ymax]
+    plt.axis([10, 1,low_price-2,high_price+2])
 
-#Display the graph
-plt.show()
+    # Set the labels for the graph
+    plt.xlabel("Days Ago")
+    plt.ylabel("Closing Price")
+    plt.title("Closing Price for " + stock)
+
+    # Display the graph
+    plt.show()
 
