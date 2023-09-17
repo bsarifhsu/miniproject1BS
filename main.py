50676c461c040d913cad4d8beb110bd01068fbcc
bsarifhsu <b_sari@mail.fhsu.edu>
2023-09-17 16:50:40 -0500

    Start of the program functions added.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def getClosing(ticker):
     return closing_list
 
 
-def generateGraph(stocks):
+def generateGraph(stock):
     # It creates the chart folder
     try:
         # Create the charts folder
@@ -29,34 +29,33 @@ def generateGraph(stocks):
         pass
 
     # Looping through the stocks and generating a plot for each
-    for stock in stocks:
-        stockClosing = np.array(getClosing(stock))
-        days = list(range(1, len(stockClosing) + 1))
+    stockClosing = np.array(getClosing(stock))
+    days = list(range(1, len(stockClosing) + 1))
 
-        # It plots the graph
-        plt.plot(days, stockClosing)
+    # It plots the graph
+    plt.plot(days, stockClosing)
 
-        # Get the min and max for Y axis
-        prices = getClosing(stock)
-        prices.sort()
-        low_price = prices[0]
-        high_price = prices[-1]
+    # Get the min and max for Y axis
+    prices = getClosing(stock)
+    prices.sort()
+    low_price = prices[0]
+    high_price = prices[-1]
 
-        # Set the X axis min and max
-        # Form [xmin,xmax, ymin, ymax]
-        plt.axis([10, 1, low_price - 2, high_price + 2])
+    # Set the X axis min and max
+    # Form [xmin,xmax, ymin, ymax]
+    plt.axis([10, 1, low_price - 2, high_price + 2])
 
-        # Set the labels for the graph
-        plt.xlabel("Days Ago")
-        plt.ylabel("Closing Price")
-        plt.title("Closing Price for " + stock)
+    # Set the labels for the graph
+    plt.xlabel("Days Ago")
+    plt.ylabel("Closing Price")
+    plt.title("Closing Price for " + stock)
 
-        # Saves plot
-        savefile = "charts/" + stock + ".png"
-        plt.savefig(savefile)
+    # Saves plot
+    savefile = "charts/" + stock + ".png"
+    plt.savefig(savefile)
 
-        # Display the graph
-        plt.show()
+    # Display the graph
+    plt.show()
 
 #Get stocks from the user
 def getStocks():
@@ -65,18 +64,23 @@ def getStocks():
 
     print("Please add 5 stocks to graph: ")
     for i in range(1,6):
-        while true:
+        while True:
             print("Enter stock ticker" + str(i))
             ticker = input("> ")
             try:
+                print("Checking Ticker")
                 stock = yf.Ticker(ticker)
                 stock.info
                 stocks.append(ticker)
+                print("Valid Ticker")
                 break
             except:
                 print("It is not a valid stock.Please enter another: ")
     return stocks
 
-
+# Start of the program
+for stock in getStocks():
+    getClosing(stock)
+    generateGraph(stock)
 
 
