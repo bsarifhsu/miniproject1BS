159fa89b1c52ca238eca0f4a4204da6dd1ef1075
bsarifhsu <b_sari@mail.fhsu.edu>
2023-09-17 16:30:03 -0500

    Get stocks from the user function added.

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-# Ticker for Sony is SONY
-# Ticker for Microsoft is MSFT
-# Ticker for Adobe is ADBE
-# Ticker for Alphabet is GOOG
-# Ticker for NVIDIA is NVDA
-
 def getClosing(ticker):
     # Get the closing price for the last trading days
     stock = yf.Ticker(ticker)
@@ -25,43 +19,64 @@ def getClosing(ticker):
         closing_list.append(round(price, 2))
     return closing_list
 
-# It creates the chart folder
-try:
-    #Create the charts folder
-    Path("charts").mkdir()
-except FileExistsError:
-    pass
+
+def generateGraph(stocks):
+    # It creates the chart folder
+    try:
+        # Create the charts folder
+        Path("charts").mkdir()
+    except FileExistsError:
+        pass
+
+    # Looping through the stocks and generating a plot for each
+    for stock in stocks:
+        stockClosing = np.array(getClosing(stock))
+        days = list(range(1, len(stockClosing) + 1))
+
+        # It plots the graph
+        plt.plot(days, stockClosing)
+
+        # Get the min and max for Y axis
+        prices = getClosing(stock)
+        prices.sort()
+        low_price = prices[0]
+        high_price = prices[-1]
+
+        # Set the X axis min and max
+        # Form [xmin,xmax, ymin, ymax]
+        plt.axis([10, 1, low_price - 2, high_price + 2])
+
+        # Set the labels for the graph
+        plt.xlabel("Days Ago")
+        plt.ylabel("Closing Price")
+        plt.title("Closing Price for " + stock)
+
+        # Saves plot
+        savefile = "charts/" + stock + ".png"
+        plt.savefig(savefile)
+
+        # Display the graph
+        plt.show()
+
+#Get stocks from the user
+def getStocks():
+
+    stocks = []
+
+    print("Please add 5 stocks to graph: ")
+    for i in range(1,6):
+        while true:
+            print("Enter stock ticker" + str(i))
+            ticker = input("> ")
+            try:
+                stock = yf.Ticker(ticker)
+                stock.info
+                stocks.append(ticker)
+                break
+            except:
+                print("It is not a valid stock.Please enter another: ")
+    return stocks
 
 
-stocks = ["MSFT","SONY","ADBE","GOOG","NVDA"]
-#Looping through the stocks and generationg a plot for each
-for stock in stocks:
-    stockClosing = np.array(getClosing(stock))
-    days = list(range(1, len(stockClosing)+1))
-
-    # It plots the graph
-    plt.plot(days, stockClosing)
-
-    # Get the min and max for Y axis
-    prices = getClosing(stock)
-    prices.sort()
-    low_price = prices[0]
-    high_price = prices[-1]
-
-    # Set the X axis min and max
-    # Form [xmin,xmax, ymin, ymax]
-    plt.axis([10, 1,low_price-2,high_price+2])
-
-    # Set the labels for the graph
-    plt.xlabel("Days Ago")
-    plt.ylabel("Closing Price")
-    plt.title("Closing Price for " + stock)
-
-    #Saves plot
-    savefile = "charts/" + stock + ".png"
-    plt.savefig(savefile)
-
-    # Display the graph
-    plt.show()
 
 
